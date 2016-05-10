982a7adf77c0e0e1750e0837c5768baa3ac32d14
Jamar Brooks <j388923r@mit.edu>
2016-05-10 02:35:17 -0400

    added avplayer for background music

--- TwoOrMore-iOS/TwoOrMore/ViewController.swift
+++ TwoOrMore-iOS/TwoOrMore/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player : AVPlayer?
+    var song : AVPlayerItem?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // put link to prayer music here instead
+        let url = NSURL(string: "https://s3.amazonaws.com/kargopolov/kukushka.mp3")
+        song = AVPlayerItem(URL: url!)
+        player = AVPlayer(playerItem: song!)
+        
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = CGRectMake(0, 0, 10, 50)
+        self.view.layer.addSublayer(playerLayer)
     }
 
+    override func viewDidAppear(animated: Bool) {
+        player!.play()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
